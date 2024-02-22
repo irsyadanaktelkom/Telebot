6f33c3a35f1f1adebf3713f83ad149a891305383
irsyadfnurdin23 <irsyadfauzan28@gmail.com>
2024-02-22 17:52:19 +0700

    Update kucoin.js

--- kucoin.js
+++ kucoin.js
@@ -1,13 +1,53 @@
-async function getData() {
-    try {
-        const currencyResponse = await fetch('https://api.kucoin.com/api/v3/currencies');
-        const currencyData = await currencyResponse.json();
+const fetch = require('node-fetch');
 
-        return { currencies: currencyData.data };
+async function getCurrencies() {
+    try {
+        const response = await fetch('65d20bd58a137d0001734757');
+        const responseData = await response.json();
+
+        // Extracting data from the response
+        const currencies = responseData.data.map(currency => {
+            const { currency: symbol, name, fullName, chains } = currency;
+            const formattedChains = chains.map(chain => {
+                const {
+                    chainName,
+                    withdrawalMinSize,
+                    withdrawalMinFee,
+                    isWithdrawEnabled,
+                    isDepositEnabled,
+                    confirms,
+                    preConfirms,
+                    contractAddress,
+                    chainId
+                } = chain;
+                return {
+                    chainName,
+                    withdrawalMinSize,
+                    withdrawalMinFee,
+                    isWithdrawEnabled,
+                    isDepositEnabled,
+                    confirms,
+                    preConfirms,
+                    contractAddress,
+                    chainId
+                };
+            });
+
+            return {
+                symbol,
+                name,
+                fullName,
+                chains: formattedChains
+            };
+        });
+
+        return currencies;
     } catch (error) {
-        console.error('Error fetching data from KuCoin:', error);
+        console.error('Error fetching currencies from KuCoin:', error);
         return null;
     }
 }
 
-module.exports = { getData };
+module.exports = {
+    getCurrencies
+};
